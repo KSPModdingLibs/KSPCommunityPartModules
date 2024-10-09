07ad189ac6b76ba468ddf7d0c0ec6482941f33d6
JonnyOThan <jonnyothan@gmail.com>
2024-10-09 14:59:33 -0400

    small tweaks

--- Source/Modules/ModuleCoPFollowTransform.cs
+++ Source/Modules/ModuleCoPFollowTransform.cs
@@ -23,16 +23,34 @@ namespace KSPCommunityPartModules.Modules
             if (followTransform == null || followTransform.name != transformName)
             {
                 if (transformName != null) followTransform = part.FindModelTransform(transformName);
-                if (followTransform == null) Debug.LogError($"[{MODULENAME}] transformName was empty or does not exist.");
-                this.isEnabled = followTransform != null;
-                this.enabled = followTransform != null;
+                if (followTransform == null) Debug.LogError($"[{MODULENAME}] transformName '{transformName}' was empty or does not exist.");
             }
+
+            if (followTransform == null)
+            {
+                // this may be important if someone is swapping out versions of this module with B9PS
+                part.CoPOffset = Vector3.zero;
+            }
+
+            // NOTE: isEnabled will be persisted to the save file, but we want to treat it purely as runtime state
+            this.isEnabled = followTransform != null;
+            this.enabled = followTransform != null && HighLogic.LoadedSceneIsFlight;
         }
 
         public void FixedUpdate()
         {
-            if (!HighLogic.LoadedSceneIsFlight) return;
-            if (followTransform != null) part.CoPOffset = part.transform.InverseTransformPoint(followTransform.position);
+            // Note that we shouldn't ever get here if the transform just didn't exist
+            // But it's certainly possible for *something* to delete it later
+            if (followTransform == null)
+            {
+                isEnabled = false;
+                enabled = false;
+                // should we reset CoPOffset here?   Not sure.
+            }
+            else    
+            {
+                part.CoPOffset = part.transform.InverseTransformPoint(followTransform.position);
+            }
         }
     }
 }
