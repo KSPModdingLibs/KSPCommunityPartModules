38832595f0337ddc2cbee4b482d16ca37745b4f6
Sofie <63377159+SofieBrink@users.noreply.github.com>
2024-11-16 20:53:48 +0100

    Code cleanup
    - Removed harmony patch for ModuleAutoCutDrogue from the module's cs file and moved it to its own harmony patches folder

---
+++ Source/HarmonyPatches/ModuleParachutePatch.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using System;
+
+namespace KSPCommunityPartModules.HarmonyPatches
+{
+    [HarmonyPatch(typeof(ModuleParachute))]
+    public class ModuleParachuteEvents
+    {
+        public static void ModuleManagerPostLoad()
+        {
+            Harmony harmony = new Harmony("KSPCommunityPartModules.ModuleParachutePatch");
+            harmony.PatchAll();
+        }
+
+        public static event Action<ModuleParachute> OnDeployed;
+        public static event Action<ModuleParachute> OnRepacked;
+
+        [HarmonyPostfix]
+        [HarmonyPatch("OnParachuteSemiDeployed")]
+        static void RaiseEventOnSemiDeployed(ModuleParachute __instance)
+        {
+            OnDeployed?.Invoke(__instance);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("OnParachuteFullyDeployed")]
+        static void RaiseEventOnFullyDeployed(ModuleParachute __instance)
+        {
+            OnDeployed?.Invoke(__instance);
+        }
+
+        [HarmonyPostfix]
+        [HarmonyPatch("Repack")]
+        static void RaiseEventOnRepack(ModuleParachute __instance)
+        {
+            OnRepacked?.Invoke(__instance);
+        }
+    }
+}
